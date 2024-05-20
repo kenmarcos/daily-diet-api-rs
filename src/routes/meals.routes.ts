6e58d5de1ea4ca33b316726df04f63e3c5e7f340
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-05-20 15:04:46 -0300

    feat: add metrics query

--- src/routes/meals.routes.ts
+++ src/routes/meals.routes.ts
@@ -59,6 +59,40 @@ export const mealsRoutes = async (app: FastifyInstance) => {
     });
   });
 
+  app.put("/:id", async (request, reply) => {
+    const editMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      date: z.coerce.date(),
+      isOnDiet: z.boolean(),
+    });
+
+    const editMealParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = editMealParamsSchema.parse(request.params);
+
+    const { name, description, date, isOnDiet } = editMealBodySchema.parse(
+      request.body
+    );
+
+    const mealById = await knex("meals").where("id", id).first();
+
+    if (!mealById) {
+      return reply.status(404).send({
+        message: "Meal not found",
+      });
+    }
+
+    await knex("meals").where("id", id).update({
+      name,
+      description,
+      date: date.toISOString(),
+      is_on_diet: isOnDiet,
+    });
+  });
+
   app.delete("/:id", async (request, reply) => {
     const deleteMealParamsSchema = z.object({
       id: z.string().uuid(),
@@ -77,5 +111,51 @@ export const mealsRoutes = async (app: FastifyInstance) => {
     await knex("meals").where("id", id).delete();
 
     return reply.status(204).send();
+  });
+
+  app.get("/metrics", async (request, reply) => {
+    const totalMeals = await knex("meals")
+      .where("user_id", request.user?.id)
+      .orderBy("date", "desc");
+
+    const totalMealsOnDiet = await knex("meals")
+      .where({
+        user_id: request.user?.id,
+        is_on_diet: true,
+      })
+      .count("id", { as: "total" })
+      .first();
+
+    const totalMealsOffDiet = await knex("meals")
+      .where({
+        user_id: request.user?.id,
+        is_on_diet: false,
+      })
+      .count("id", { as: "total" })
+      .first();
+
+    const { bestOnDietSequence } = totalMeals.reduce(
+      (acc, meal) => {
+        if (meal.is_on_diet) {
+          acc.currentSequence += 1;
+        } else {
+          acc.currentSequence = 0;
+        }
+
+        if (acc.bestOnDietSequence < acc.currentSequence) {
+          acc.bestOnDietSequence = acc.currentSequence;
+        }
+
+        return acc;
+      },
+      { bestOnDietSequence: 0, currentSequence: 0 }
+    );
+
+    return reply.send({
+      totalMeals: totalMeals.length,
+      totalMealsOnDiet: totalMealsOnDiet?.total,
+      totalMealsOffDiet: totalMealsOffDiet?.total,
+      bestOnDietSequence,
+    });
   });
 };
